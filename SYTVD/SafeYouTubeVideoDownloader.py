face3347717c6b78d1905f4cb452abacd2c24fd2
Yuskiv <pavloyuskiv@gmail.com>
2017-05-27 00:21:26 +0300

    Add files via upload

---
+++ SYTVD/SafeYouTubeVideoDownloader.py
@@ -0,0 +1,27 @@
+import important_dont_delete
+video_link = 'https://www.youtube.com/watch?v=l5pregTudRA'
+yt = important_dont_delete.YouTube(video_link)
+videos = yt.get_videos()
+print('Information about video:')
+print('FORMATS & SIZES:')
+for v in videos:
+    print(v)
+
+
+first_video = videos[0]
+print(first_video)
+
+first_video.extension
+
+
+audio_bitrate = first_video.audio_bitrate
+video_bitrate = first_video.video_bitrate
+print('audio bitrate: ',audio_bitrate)
+print('video bitrate: ',video_bitrate)
+
+
+videos = yt.videos
+video = yt.get('3gp', '144p')
+
+path = '/Users/Fazan/Desktop'
+video.download(path)
