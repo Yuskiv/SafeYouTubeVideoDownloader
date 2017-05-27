daaaedc9e4f70826262199d18850f13269753017
Yuskiv <pavloyuskiv@gmail.com>
2017-05-27 18:29:29 +0300

    Update __init__.py

--- SYTVD/important_dont_delete/__init__.py
+++ SYTVD/important_dont_delete/__init__.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# flake8: noqa
+
 __title__ = 'important_dont_delete'
-__version__ = '6.2.2'
-__author__ = 'Pavlo Yuskiv'
 
 from .api import YouTube
 
-# Set default logging handler to avoid "No handler found" warnings.
 import logging
 try:
     from logging import NullHandler
